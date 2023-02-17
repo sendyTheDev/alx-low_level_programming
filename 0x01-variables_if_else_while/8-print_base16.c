8caebd5ebd749ef13b22476761eba41dd2bc9359
sendyTheDev <phathisanisendy@gmail.com>
2023-02-17 14:51:14 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - base 10 numbers followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
