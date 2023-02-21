cf99c146eab5ef8a2074f10863940905f106265e
sendyTheDev <phathisanisendy@gmail.com>
2023-02-21 05:10:29 -0800

    0. _putcha

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - program that prints _putchar
+ * @c: The character to print
+ *
+ * Return: 0
+ */
+int _putchar(char c)
+{
+	return (write(1, &c 1));
+}
