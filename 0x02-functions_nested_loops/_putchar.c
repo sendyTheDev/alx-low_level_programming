aa46a149d75e79559b5e0316ff889f5c47fcff77
sendyTheDev <phathisanisendy@gmail.com>
2023-02-21 06:22:59 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,9 +4,10 @@
  * main - program that prints _putchar
  * @c: The character to print
  *
- * Return: 0
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return (write(1, &c 1));
+	return (write(1, &c, 1));
 }
