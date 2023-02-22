3764131693fec914a6b590f1a1dbd14c236620ca
sendyTheDev <phathisanisendy@gmail.com>
2023-02-22 04:35:39 -0800

    102 fibonanacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: computes and print the sum of all multiples
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	long int a = 0;
+	long int b = 1;
+	long int next;
+
+	while (i < 50)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(",");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
