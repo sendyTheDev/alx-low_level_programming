5a78fe0239396b0771112a6f5c774e52d6499c12
sendyTheDev <phathisanisendy@gmail.com>
2023-02-22 15:02:12 -0800

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+	unsigned long int a, b, c;
+	int i;
+
+	a = 0;
+	b = 1;
+	c = 1;
+
+	for (i = 0; i < 98; i++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		printf("%lu", c);
+
+		if (i != 97)
+		{
+			printf(",");
+		}
+	}
+	printf("\n");
+	return (0);
+}
