e1e1512417d04b9f2ed70b7ec6df08f1d7bad01b
sendyTheDev <phathisanisendy@gmail.com>
2023-04-04 12:07:58 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 /**
  * print_listint - it prints the element of linked list
  * @h: linked list of typer listint_t to print
- * Return: number of nodes
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
