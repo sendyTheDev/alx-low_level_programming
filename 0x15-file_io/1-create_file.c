93257b00342e1dd762ab369186b92f5381770df5
sendyTheDev <phathisanisendy@gmail.com>
2023-04-09 16:33:53 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[words] != '\0)
+		while (text_content[words] != '\0')
 			words++;
 
 		write_status = write(file, text_content, words);
